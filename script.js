6d77962e682c265ef44673a7ed49c3eb0e0b7936
nsamedaisy <nsamedaisy@gmail.com>
2023-05-11 17:06:28 +0100

    github profile commit

--- script.js
+++ script.js
@@ -4,28 +4,33 @@ const form = document.getElementById('form')
 const search = document.getElementById('search')
 const main = document.getElementById('main')
 
-async function getUser(username) {
+// we will use axios to handle our request through async/await
+// Axios is an open-source, promise-based HTTP client. It uses JS promises to send HTTP requests and manage their responses and it fetches APIs that are not available in the basic Fetch API.
+
+// Axios request with async/await pauses the asynchronous function untill the promise is resolved
+
+// the Try...catch block is use to handle errors in standard API call using axios
+
+async function getUser (username) {
   try {
-    const {data} = await axios(APIURL + username)
+    const { data } = await axios(APIURL + username)
 
     createUserCard(data)
     getRepos(username)
-  }
-  catch (err) {
-    if(err.response.status == 404) {
+  } catch (err) {
+    if (err.response.status === 404) {
       createErrorCard('No profile with this user name')
     }
   }
 }
 
-async function getRepos(username) {
+async function getRepos (username) {
   try {
-    const {data} = await axios(APIURL + username + '/repos?sort=created')
+    const { data } = await axios(APIURL + username + '/repos?sort=created') // sort=created get the latest repos
 
     addReposToCard(data)
-  }
-  catch (err) {
-      createErrorCard('Problem fetching repos')
+  } catch (err) {
+    createErrorCard('Problem fetching repos')
   }
 }
 
@@ -33,13 +38,15 @@ function createUserCard (user) {
   const cardHTML = `
   <div class="card">
   <div>
-    <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
+<a href="http://github.com/users/"><img src="${user.avatar_url}" alt="${user.name}" class="avatar"></a>
   </div>
   <div class="user-info">
     <h2>${user.name}</h2>
     <p>${user.bio}</p>
     <ul>
-      <li>${user.followers} <strong>Followers</strong></li>
+<a href="http://github.com">
+<li>${user.followers} <strong>Followers</strong></li>
+</a>
       <li>${user.following} <strong>Following</strong></li>
       <li>${user.public_repos} <strong>Repos</strong></li>
     </ul>
@@ -51,7 +58,7 @@ function createUserCard (user) {
   main.innerHTML = cardHTML
 }
 
-function createErrorCard(mgs) {
+function createErrorCard (mgs) {
   const cardHTML = `
   <div class= "card">
     <h1>${mgs}</h1>
@@ -61,16 +68,19 @@ function createErrorCard(mgs) {
   main.innerHTML = cardHTML
 }
 
-function addReposToCard(repos) {
+function addReposToCard (repos) {
   const reposEl = document.getElementById('repos')
 
   repos // looping tru my repo
-    .slice(0, 3)
+
+    .slice(0, 3) // slice the repo array to a desired number
+
+  // for each repos we create our links
     .forEach(repo => {
       const repoEl = document.createElement('a')
       repoEl.classList.add('repo')
-      repoEl.href = repo.html_url
-      repoEl.target = '_blank'
+      repoEl.href = repo.html_url // points to the actual repository url
+      repoEl.target = '_blank' // opens in a new window
       repoEl.innerText = repo.name
 
       reposEl.appendChild(repoEl)
@@ -78,6 +88,7 @@ function addReposToCard(repos) {
 }
 
 form.addEventListener('submit', (e) => {
+  // prevents the auto submission of the form
   e.preventDefault()
 
   const user = search.value
